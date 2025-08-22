b57d96c0ca2b24458e0051c74cbdfeb3ae02318b
Garkatron <145294535+Garkatron@users.noreply.github.com>
2025-08-22 14:06:29 +0200

    I like it

---
+++ src/components/images.js
@@ -0,0 +1,21 @@
+import ProfilePhoto from '../assets/images/photo.jpg';
+import Renderer3D from '../assets/images/renderer3d.png';
+import Chip8 from '../assets/images/chip8image.png';
+import PokeApi from '../assets/images/pokeapi.png';
+import VonNeumann from '../assets/images/vonneuman.png';
+import Guidebook from '../assets/images/guidebook.png';
+import Stanley from '../assets/images/stanley.png';
+import Paperwork from '../assets/images/paperwork.png';
+import Parkour from '../assets/images/parkour.png';
+
+export {
+    ProfilePhoto,
+    Renderer3D,
+    Chip8,
+    PokeApi,
+    VonNeumann,
+    Guidebook,
+    Stanley,
+    Paperwork,
+    Parkour
+};

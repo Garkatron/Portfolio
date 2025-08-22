d70a256799e49a2ef834fea2d157e29666e12649
Garkatron <145294535+Garkatron@users.noreply.github.com>
2025-08-22 13:01:40 +0200

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     site: 'https://Garkatron.github.io',
     base: '/Portfolio/',
+    vite: {
+        plugins: [tailwindcss()],
+    },
 })
 export const BASE_PATH = {
     BASE_PATH: "/app"

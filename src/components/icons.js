347d13f74a446ff01a13d98ec07c87e22b513b10
Garkatron <145294535+Garkatron@users.noreply.github.com>
2025-09-18 14:27:10 +0200

    Revert "Changed to comic style"
    This reverts commit eab7571a4db17c70d896a3eb3111286be37f7b2c.

--- src/components/icons.js
+++ src/components/icons.js
@@ -1,17 +1,13 @@
 // Languages
 import JavaIcon from '../assets/icons/java-svgrepo-com.svg';
-import TsIcon from '../assets/icons/typescript-official-svgrepo-com.svg';
-import MongoDb from '../assets/icons/mongo-svgrepo-com.svg';
 import JsIcon from '../assets/icons/js-svgrepo-com.svg';
 import KotlinIcon from '../assets/icons/kotlin-svgrepo-com.svg';
 import RustIcon from '../assets/icons/rust-svgrepo-com.svg';
 import PythonIcon from '../assets/icons/python-svgrepo-com.svg';
-import FramerMotion from '../assets/icons/framer-motion.svg';
 
 // Frameworks & Libraries
 import BootstrapIcon from '../assets/icons/bootstrap-4-logo-svgrepo-com.svg';
 import AstroIcon from '../assets/icons/astro-svgrepo-com.svg';
-import React from '../assets/icons/reactjs-svgrepo-com.svg';
 
 // Tools
 import GitIcon from '../assets/icons/git-svgrepo-com.svg';
@@ -31,5 +27,5 @@ export {
     BootstrapIcon, AstroIcon,
     GitIcon, PostmanIcon,
     LinuxIcon, SqlIcon,
-    CvIcon, GithubIcon, MailIcon, React, TsIcon, MongoDb, FramerMotion
+    CvIcon, GithubIcon, MailIcon
 };
